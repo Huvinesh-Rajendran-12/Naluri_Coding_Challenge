ac1bb4f021465486b79b2875f1c2153aa789b33a
Huvinesh Rajendran <81321926+Huvinesh-Rajendran-12@users.noreply.github.com>
2022-08-04 14:35:48 +0800

    implemented circumference calculation

--- naluri_backend/controllers/functions.js
+++ naluri_backend/controllers/functions.js
@@ -1,3 +1,4 @@
+// function to calculate the Pi value
 const calculatePI = async (PI, n, sign) => {
    // The number of iteratios is set to 1000000 for better accuracy
    for (let i = 0; i <= 1000000; i++) {
@@ -13,17 +14,34 @@ const calculatePI = async (PI, n, sign) => {
     // Return the value of Pi
     return PI;
 }
+// function to calculate the circumference of a circle
+const CALCULATE_CIRCUMFERENCE = async (pi, radius) => {
+    try{
+        const circumference = pi * 2 * radius;
+        return circumference;
+    } catch(err){
+        console.log(err);
+    }
+}
 
-const CALCULATE_PI = async () => new Promise(async(resolve) => {
+// calculate the value of PI and circumference of the sun
+console.time('calculatePI');
+const CALCULATE_PI_CIRCUMFERENCE = async () => new Promise(async(resolve) => {
     try{
         const pi = await calculatePI(3, 2, 1);
-        resolve(pi);
+        const circumference = await CALCULATE_CIRCUMFERENCE(pi, 696340);
+        const info = {
+            pi,
+            circumference
+        }
+        resolve(info);
     } catch(err){
         console.log(err);
     }
 }
 );
+console.timeEnd('calculatePI');
 
 module.exports = {
-    CALCULATE_PI
+    CALCULATE_PI_CIRCUMFERENCE
 }
--- naluri_backend/routes/routes.js
+++ naluri_backend/routes/routes.js
@@ -1,5 +1,5 @@
 // import the CALCULATE_PI method from the functions.js file
-const { CALCULATE_PI } = require('../controllers/functions');
+const { CALCULATE_PI_CIRCUMFERENCE } = require('../controllers/functions');
 // import the express module
 const express = require('express');
 // define the router
@@ -8,10 +8,11 @@ const router = express.Router();
 // define get method to get the value of PI
 router.get('/getPI', async (req, res) => {
     try {
-        const pi = await CALCULATE_PI();
+        const data = await CALCULATE_PI_CIRCUMFERENCE();
         res.status(200).json({
             message: 'Successfully retrieved PI',
-            pi
+            pi: data.pi,
+            circumference: data.circumference,
         });
     } catch (err) {
         res.status(500).json({
